4e82395767a7815bea4d1b0de0b842b4b6e89e76
bloodycloud1 <sparky.1995@mail.ru>
2023-11-20 00:30:49 +0500

    first commit

---
+++ src/components/dish-section/dish.tsx
@@ -0,0 +1,11 @@
+// const Dish = () => {
+//   return (
+//     <div className="	">
+//       <div>689 р</div>
+//       <div>Мясная бомба</div>
+//       <div>520 г</div>
+//       <button>Добавить</button>
+//     </div>
+//   );
+// };
+// export default Dish;
